1ed123596cf9cfe6c9d86f3c8a5b5cc5a1b0a859
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2023-05-11 16:24:38 +0600

    add microutils and support of native platforms

--- src/commonMain/kotlin/dev/inmo/saucenaoapi/utils/MPPFile.kt
+++ src/commonMain/kotlin/dev/inmo/saucenaoapi/utils/MPPFile.kt
@@ -1,9 +1,26 @@
 package dev.inmo.saucenaoapi.utils
 
+import dev.inmo.micro_utils.common.MPPFile
+import dev.inmo.micro_utils.common.filename
+import dev.inmo.micro_utils.ktor.common.input
+import dev.inmo.micro_utils.mime_types.KnownMimeTypes
+import dev.inmo.micro_utils.mime_types.getMimeType
 import io.ktor.http.ContentType
 import io.ktor.utils.io.core.Input
 
-expect class MPPFile
+@Deprecated(
+    "MPPFile from microutils is preferable since 0.16.0",
+    ReplaceWith("MPPFile", "dev.inmo.micro_utils.common.MPPFile")
+)
+typealias MPPFile = MPPFile
 
-expect val MPPFile.input: Input
-expect val MPPFile.contentType: ContentType
+@Deprecated(
+    "input() from microutils is preferable since 0.16.0",
+    ReplaceWith("this.input()", "dev.inmo.micro_utils.ktor.common.input")
+)
+val MPPFile.input: Input
+    get() = input()
+val MPPFile.contentType: ContentType
+    get() = ContentType.parse(
+        getMimeType(stringWithExtension = filename.extension) ?.raw ?: KnownMimeTypes.Any.raw
+    )
